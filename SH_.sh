0823bffba89fc3aabc9ca30f48ed3cfe8be0ee71
Salpeji.Salpeji <Salpeji.samuelrezpe@gmail.com>
2021-09-06 00:12:55 -0600

    Proyecto1_BD cargas

---
+++ SH_.sh
@@ -0,0 +1,9 @@
+sqlldr BD_VideoTeca/Sam26 control=C:\\Users\\samue\\Desktop\\BD_PROY_1\\TEMPORAL.ctl
+data=C:\\Users\\samue\\Desktop\\BD_PROY_1\\TEMPORAL.txt
+log=C:\\Users\\samue\\Desktop\\BD_PROY_1\\TEMPORAL.log
+bad=C:\\Users\\samue\\Desktop\\BD_PROY_1\\TEMPORAL.bad
+discard=C:\\Users\\samue\\Desktop\\BD_PROY_1\\TEMPORAL.dsd
+
+echo " "
+echo -e "\e[96m  Presionar Cualquier Tecla para Continuar ... \e[0m"
+read
